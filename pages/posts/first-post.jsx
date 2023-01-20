73f4697898121c9521378953c4ddcabf8d823c0d
Rosanne Schönig <rosanne.schoenig@gmx.de>
2023-01-20 12:28:18 +0100

    first commit

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,13 @@
+export default function FirstPost() {
+    return (
+        <div>
+            <h1>This is my First Post</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+                totam tempora et esse unde dolor itaque soluta at voluptas,
+                facilis aspernatur vero id alias, provident assumenda porro
+                repellat fugit inventore!
+            </p>
+        </div>
+    );
+}
