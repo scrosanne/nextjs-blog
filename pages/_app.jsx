f491d39781f549796bc3d5e785027bb56d47ee34
Rosanne Schönig <rosanne.schoenig@gmx.de>
2023-01-23 09:25:41 +0100

    blog layout, dynamic with data fetching

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import "@/styles/globals.css";
+
+//font
+import { Inter } from "@next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }) {
+    return (
+        <div className={inter.className}>
+            <Component {...pageProps} />;
+        </div>
+    );
+}
--- pages/about.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-    return (
-        <div>
-            <h1>about levhausen</h1>
-        </div>
-    );
-}
